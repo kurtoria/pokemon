5f1c98803065268d0aa3b5acdf3582356872c582
Moa Lenngren <moa.mail@hotmail.com>
2018-08-20 13:42:08 +0200

    Added express

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+var express = require('express')
+var app = express()
+
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      /*  <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Ät bajs moa</Text>
+        <Text>Shake your phone to open the developer menu.</Text> */
+        
       </View>
     );
   }
